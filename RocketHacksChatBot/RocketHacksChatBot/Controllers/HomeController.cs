c6929b00058548ef59b3c4db26a6c67f082944af
Noah Moorhouse <noahjmoorhouse@outlook.com>
2025-03-15 13:41:05 -0400

    add bootstrap

---
+++ RocketHacksChatBot/RocketHacksChatBot/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OllamaSharp.Models.Chat;
+using RocketHacksChatBot.Services;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RocketHacksChatBot.Controllers
+{
+    [ApiController]
+[Route("api/[controller]")]
+    public class testController : Controller
+    {
+        [HttpGet("test")]
+        public string test()
+        {
+            CallOllama callOllama = new CallOllama();
+
+            ChatRequest chatRequest = new ChatRequest
+            {
+                mess
+            };
+
+            callOllama.AIChat()
+        }
+    }
+}
+
---
+++ RocketHacksChatBot/RocketHacksChatBot/Services/CallOllama.cs
@@ -0,0 +1,25 @@
+﻿
+
+using OllamaSharp;
+using RocketHacksChatBot.Models;
+
+namespace RocketHacksChatBot.Services
+{
+    public class CallOllama
+    {
+       public async void AIChat (AIChatRequest request)
+        {
+            var uri = new Uri("http://localhost:11434");
+            var ollama = new OllamaApiClient(uri);
+            var chat = new Chat(ollama);
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+                await foreach (var answerToken in chat.SendAsync(request.message))
+                    Console.Write(answerToken);
+            }
+
+        }
+    }
+}
